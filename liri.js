72db6ac2d83b21d6b10f31e0ff3284cc085f1176
Daniel <dhuerta1408@gmail.com>
2019-10-02 21:11:02 -0600

    more functionality added

--- liri.js
+++ liri.js
@@ -21,18 +21,18 @@ var value = process.argv[3];
 
 switch (command) {
     case "concert-this":
-        getBands(value)
+        bands(value)
         break;
     
     case "spotify-this-song":
-        getSongs(value)
+        songs(value)
         break;
 
     case "movies-this":
         if (value == "") {
             value = defaultMovie;
         }
-        getMovies(value)
+        movies(value)
         break;
 
     case "do-what-it-says":
@@ -43,7 +43,7 @@ switch (command) {
         break;
 }
 
-function getBands(artist) {
+function bands(artist) {
 
 axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
     .then(function (response) {
@@ -59,6 +59,66 @@ axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbo
     .catch(function (err) {
         console.log(err);
     });
-
 }
+
+function songs(trackName) {
+
+if (trackName === "") {
+    trackName = "L$D"
+}
+
+spotify.search({type: 'track', query: trackName },function (err, data) {
+
+    if(err) {
+        console.log("Error: " + err);
+    }
+
+    console.log("Artist: ", data.tracks.items[0].album.artists[0].name);
+    console.log("Preview: ", data.tracks.items[0].preview_url);
+    console.log("Album: ", data.tracks.items[0].album.name);
+});
+}
+
+function movies(movieTitle) {
+
+axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${movieTitle}`)
+    .then(function (response) {
+
+        var results = `Title: ${response.data.Title} \nRelease Year: ${response.data.Year} \n Imdb Rating: ${response.data.Rating} \nRotten Tomatoes Rating: ${response.data.Rating[1].Value} Plot: ${response.data.Plot} \nActors: ${response.data.Actors}`;
+
+        console.log(results)
+
+    }) .catch(function (err) {
+        console.log(err)
+    });
+
+    if (movieTitle === "Interstellar") {
+        console.log("The best movie of all time!!!")
+    };
+
+} 
  
+function readFile() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        data = data.split(",");
+
+        var response = data[0];
+        var value = data[1];
+
+        switch (response) {
+            case "concert-this":
+                bands(value)
+                break;
+
+            case "spotify-this-song":
+                songs(value)
+                break;
+
+            case "movies-this":
+                movies(value)
+                break;
+
+        }
+
+    })
+}
